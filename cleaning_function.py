d48c1ffa5857839c952e676bf650b5539fe686bb
Akash Bhople <akash.bhople@gmail.com>
2016-06-04 14:44:42 +0530

    Delete cleaning_function.py

--- cleaning_function.py
+++
@@ -1,5 +0,0 @@
-import re
-def text_to_words( raw_text ):        
-    letters_only = re.sub("[^a-zA-Z]", " ", raw_text) 
-    words = letters_only.lower().split()                             
-    return( " ".join(words))
